8b82feb7dc8b957e256f60572907f458e435583b
Marko Ristin <marko.ristin@gmail.com>
2021-05-26 16:53:28 +0200

    Kick off
    This is the very first version of the meta model.
    Mind that the meta-model is still a work in progress, so this version is
    neither complete nor valid.
    
    The current meta-model should serve only as a starting point for further
    collaboration.

---
+++ aas_core3_meta/verification.py
@@ -0,0 +1,41 @@
+"""
+Provide stubs for verification methods.
+
+The methods listed here will not be automatically transpiled, and their implementation
+needs to be provided in form of snippets.
+
+The stubs provide merely the type annotations and hint about the logic.
+"""
+from typing import TypeVar, Iterable, Set
+
+
+def is_IRI(text: str) -> bool:
+    raise NotImplementedError()
+
+
+def is_IRDI(text: str) -> bool:
+    raise NotImplementedError()
+
+
+def is_ID_short(text: str) -> bool:
+    raise NotImplementedError()
+
+
+T = TypeVar('T')
+
+
+def are_unique(iterable: Iterable[T]) -> bool:
+    """Return ``True`` if all the elements in the sequence are unique."""
+    observed = set()  # type: Set[T]
+    count = 0
+
+    for item in iterable:
+        observed.add(item)
+        count += 1
+
+    return len(observed) == count
+
+
+def is_of_type(value: 'Value_data_type', value_type: 'Data_type_def') -> bool:
+    # This is only a stub to hint how this function is used.
+    raise NotImplementedError()
