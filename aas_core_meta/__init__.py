fca93a105ba484ac51a36c8041ccdc8bc33ead1b
Marko Ristin <marko.ristin@gmail.com>
2021-11-17 16:22:52 +0100

    Release 2021.11.20a2 (#10)
    This is a pre-release to test publishing to PyPI.

--- aas_core_meta/__init__.py
+++ aas_core_meta/__init__.py
@@ -1,6 +1,6 @@
 """Provide meta-models for Asset Administration Shell information model."""
 
-__version__ = "2021.11.20a1"
+__version__ = "2021.11.20a2"
 __author__ = (
     "Nico Braunisch, Marko Ristin, Robert Lehmann, Marcin Sadurski, Manuel Sauer"
 )
