72f066143bfee6c7fceebf3717f5b9a0c835092a
Nico Braunisch <nico.braunisch@tu-dresden.de>
2022-02-18 13:25:02 +0100

    Update aas_core_meta/v3rc1.py
    Co-authored-by: Marko Ristin <marko.ristin@gmail.com>

--- aas_core_meta/v3rc1.py
+++ aas_core_meta/v3rc1.py
@@ -2428,8 +2428,9 @@ class Value_reference_pair(DBC):
 @reference_in_the_book(
     section=(4, 8, 2),
     index=1,
-    fragment="4.8.2%20Predefined%20Templates%20for%20Property"
-    "%20and%20Value%20Descriptions",
+    fragment=(
+        "4.8.2 Predefined Templates for Property and Value Descriptions"
+    ),
 )
 class Value_list(DBC):
     """
