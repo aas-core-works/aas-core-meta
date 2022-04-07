c52b4db49a2d2f477391aa56bb7842830029d52f
Marko Ristin <marko.ristin@gmail.com>
2022-04-07 12:11:26 +0200

    Release 2022.4.30a2 (#56)
    This is a pre-release version.
    
    * Remove redundant nullability checks in V3RC02 (#55)
    * Fix docstring for ``matches_BCP_47`` in V3RC02 (#54)

--- aas_core_meta/__init__.py
+++ aas_core_meta/__init__.py
@@ -1,6 +1,6 @@
 """Provide meta-models for Asset Administration Shell information model."""
 
-__version__ = "2022.4.30a1"
+__version__ = "2022.4.30a2"
 __author__ = "Nico Braunisch, Marko Ristin, Marcin Sadurski"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Alpha"
