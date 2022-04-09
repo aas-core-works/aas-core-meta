1ee28c700458ca97dd41a9a7a22cdc5c8128a537
Marko Ristin <marko.ristin@gmail.com>
2022-04-09 09:47:31 +0200

    Release 2022.4.30a5
    This is a pre-release version.
    
    * Fix dangling references in V3RC02 (#63)

--- aas_core_meta/__init__.py
+++ aas_core_meta/__init__.py
@@ -1,6 +1,6 @@
 """Provide meta-models for Asset Administration Shell information model."""
 
-__version__ = "2022.4.30a4"
+__version__ = "2022.4.30a5"
 __author__ = "Nico Braunisch, Marko Ristin, Marcin Sadurski"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Alpha"
