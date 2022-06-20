62c3ad1b0461650142e9fe1bfb6b71bec57f33bf
Marko Ristin <marko.ristin@gmail.com>
2022-06-20 17:09:04 +0200

    Release 2022.6.20 (#120)
    This is a minor bug-fix version. We release it so we can start working
    on tests with XML data.
    
    * Enforce Extension value match type in V3RC02 (#119)
    * Revert back AASd-119 in V3RC02 (#118)

--- aas_core_meta/__init__.py
+++ aas_core_meta/__init__.py
@@ -1,6 +1,6 @@
 """Provide meta-models for Asset Administration Shell information model."""
 
-__version__ = "2022.6.19"
+__version__ = "2022.6.20"
 __author__ = "Nico Braunisch, Marko Ristin, Marcin Sadurski"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Alpha"
