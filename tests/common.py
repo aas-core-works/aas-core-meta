8ebb211794baa9446af8472a5e0bb4db1ee780e7
Sebastian Heppner <s.heppner@plt.rwth-aachen.de>
2023-01-25 10:08:38 +0100

    Write first draft of V3 of the meta-model
    This is the initial version of the formalized V3 meta-model translated
    from the book.
    
    The book has not been published yet! This version is intended only for
    the review.

--- tests/common.py
+++ tests/common.py
@@ -1,7 +1,7 @@
 """Provide common functionalities used across the tests."""
 import io
 import pathlib
-from typing import Tuple, MutableMapping, List
+from typing import Tuple, MutableMapping, List, Final, Set, Union
 
 import aas_core_codegen.common
 import aas_core_codegen.parse
@@ -10,13 +10,35 @@ import asttokens
 from aas_core_codegen import intermediate, infer_for_schema
 
 
-def load_atok_symbol_table_and_infer_constraints_for_schema(
+class MetaModel:
+    #: Tokens of the original meta-model
+    atok: Final[asttokens.ASTTokens]
+
+    #: Symbol table parsed from a meta-model
+    symbol_table: Final[intermediate.SymbolTable]
+
+    #: Inferred constraints grouped by class
+    constraints_by_class: Final[
+        MutableMapping[intermediate.ClassUnion, infer_for_schema.ConstraintsByProperty]
+    ]
+
+    def __init__(
+        self,
+        atok: asttokens.ASTTokens,
+        symbol_table: intermediate.SymbolTable,
+        constraints_by_class: MutableMapping[
+            intermediate.ClassUnion, infer_for_schema.ConstraintsByProperty
+        ],
+    ) -> None:
+        """Initialize with the given values."""
+        self.atok = atok
+        self.symbol_table = symbol_table
+        self.constraints_by_class = constraints_by_class
+
+
+def load_meta_model(
     model_path: pathlib.Path,
-) -> Tuple[
-    asttokens.ASTTokens,
-    intermediate.SymbolTable,
-    MutableMapping[intermediate.ClassUnion, infer_for_schema.ConstraintsByProperty],
-]:
+) -> MetaModel:
     """
     Load the symbol table from the meta-model and infer the schema constraints.
 
@@ -126,7 +148,7 @@ def load_atok_symbol_table_and_infer_constraints_for_schema(
 
     assert constraints_by_class is not None
 
-    return atok, ir_symbol_table, constraints_by_class
+    return MetaModel(atok, ir_symbol_table, constraints_by_class)
 
 
 def human_readable_property_name(name: str) -> str:
@@ -162,3 +184,117 @@ def human_readable_property_name(name: str) -> str:
             human_readable_parts.append(part)
 
     return " ".join(human_readable_parts)
+
+
+def assert_subclasses_correspond_to_enumeration_literals(
+    symbol_table: intermediate.SymbolTable,
+    cls: Union[intermediate.ConcreteClass, intermediate.AbstractClass],
+    enumeration_or_set: Union[
+        intermediate.Enumeration, intermediate.ConstantSetOfEnumerationLiterals
+    ],
+) -> None:
+    """
+    Check that all the subclasses (including the class itself) are
+    represented in the enumeration.
+    """
+    errors = []  # type: List[str]
+
+    class_name_set = set()  # type: Set[aas_core_codegen.common.Identifier]
+
+    for our_type in symbol_table.our_types:
+        if not isinstance(
+            our_type, (intermediate.AbstractClass, intermediate.ConcreteClass)
+        ):
+            continue
+
+        # We also include ``Identifiable``.
+        if our_type.is_subclass_of(cls):
+            class_name_set.add(our_type.name)
+
+    literal_set = {literal.name for literal in enumeration_or_set.literals}
+
+    if class_name_set != literal_set:
+        errors.append(
+            f"""\
+The sub-classes of {cls.name} do not correspond to {enumeration_or_set.name}.
+
+Observed classes:  {sorted(class_name_set)!r}
+Observed literals: {sorted(literal_set)!r}"""
+        )
+
+    if len(errors) != 0:
+        raise AssertionError("\n".join(f"* {error}" for error in errors))
+
+
+def assert_all_lists_have_min_length_at_least_one(
+    symbol_table: intermediate.SymbolTable,
+    constraints_by_class: MutableMapping[
+        intermediate.ClassUnion, infer_for_schema.ConstraintsByProperty
+    ],
+) -> None:
+    """Assert that all lists are constrained to at least one item."""
+    # List of (property reference, error message)
+    errors = []  # type: List[Tuple[str, str]]
+
+    for our_type in symbol_table.our_types:
+        if not isinstance(
+            our_type, (intermediate.AbstractClass, intermediate.ConcreteClass)
+        ):
+            continue
+
+        constraints_by_prop = constraints_by_class.get(our_type, None)
+
+        for prop in our_type.properties:
+            if isinstance(
+                intermediate.beneath_optional(prop.type_annotation),
+                intermediate.ListTypeAnnotation,
+            ):
+                if constraints_by_prop is None:
+                    errors.append(
+                        (
+                            f"{our_type.name}.{prop.name}",
+                            f"Inferred no constraints for our type {our_type.name}",
+                        )
+                    )
+                    continue
+
+                len_constraints = constraints_by_prop.len_constraints_by_property.get(
+                    prop, None
+                )
+                if len_constraints is None:
+                    errors.append(
+                        (
+                            f"{our_type.name}.{prop.name}",
+                            "Inferred no length constraints for the property",
+                        )
+                    )
+                    continue
+
+                if len_constraints.min_value is None:
+                    errors.append(
+                        (
+                            f"{our_type.name}.{prop.name}",
+                            "Inferred no minimum length constraint for the property",
+                        )
+                    )
+                    continue
+
+                if len_constraints.min_value < 1:
+                    errors.append(
+                        (
+                            f"{our_type.name}.{prop.name}",
+                            f"Inferred the minimum length constraints of "
+                            f"{len_constraints.min_value}",
+                        )
+                    )
+                    continue
+
+    if len(errors) != 0:
+        joined_errors = "\n".join(
+            f"* {prop_ref}: {message}" for prop_ref, message in errors
+        )
+        raise AssertionError(
+            f"Expected to infer the minimum length for the following lists "
+            f"as at least 1, but:\n"
+            f"{joined_errors}"
+        )
