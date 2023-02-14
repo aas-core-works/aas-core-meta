8dc6368fc905b5981142290e8ce5a7164babb063
Sebastian Heppner <s.heppner@plt.rwth-aachen.de>
2023-02-14 10:09:48 +0100

    Add missing value of Asset_kind enum. See #206

--- aas_core_meta/v3.py
+++ aas_core_meta/v3.py
@@ -2247,6 +2247,11 @@ class Asset_kind(Enum):
     Instance asset
     """
 
+    NotApplicable = "NotApplicable"
+    """
+    Neither a type asset nor an instance asset
+    """
+
 
 @reference_in_the_book(section=(5, 7, 4), index=3)
 @invariant(
