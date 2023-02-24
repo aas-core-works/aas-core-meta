98e25fa54db2784ad2ecbe46b3c7d7fbab179bb3
Marko Ristin <marko@ristin.ch>
2023-02-24 14:19:26 +0100

    Update aas_core_meta/v3.py

--- aas_core_meta/v3.py
+++ aas_core_meta/v3.py
@@ -1262,7 +1262,7 @@ class Identifier(Non_empty_XML_serializable_string, DBC):
     lambda self: len(self) <= 2000,
     "ValueTypeIec61360 shall have a maximum length of 2000 characters.",
 )
-class Value_type_IEC_61360(Non_empty_XML_serializable_string, DBC):
+class Value_type_IEC_61360(Non_empty_XML_serializable_string):
     """
     string
     """
