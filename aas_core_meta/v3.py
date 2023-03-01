2fe273fa228a94043354dcf4e08bd75f5fb8a1a1
Marko Ristin <marko@ristin.ch>
2023-03-01 17:16:41 +0100

    [V3] Fix version and revision types (#234)
    The patterns in version and revision type had slashes as invalid
    prefixes and suffixes.
    
    This patch fixes the issue.

--- aas_core_meta/v3.py
+++ aas_core_meta/v3.py
@@ -105,7 +105,7 @@ def matches_version_type(text: str) -> bool:
     """
     Check that :paramref:`text` is a valid version string.
     """
-    pattern = f"/^([0-9]|[1-9][0-9]*)$/"
+    pattern = f"^(0|[1-9][0-9]*)$"
 
     return match(pattern, text) is not None
 
@@ -115,7 +115,7 @@ def matches_revision_type(text: str) -> bool:
     """
     Check that :paramref:`text` is a valid revision string.
     """
-    pattern = f"/^([0-9]|[1-9][0-9]*)$/"
+    pattern = f"^(0|[1-9][0-9]*)$"
 
     return match(pattern, text) is not None
 
