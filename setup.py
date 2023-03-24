403f45d5709e84dd2b4e691f0d546b95efeb2967
Marko Ristin <marko.ristin@gmail.com>
2023-03-24 14:47:44 +0100

    right version of aas-core-codegen

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
             "mypy==0.910",
             "pylint==2.17.0",
             "asttokens>=2.0.8,<3",
-            "aas-core-codegen@git+https://github.com/aas-core-works/aas-core-codegen@b2c0e4a#egg=aas-core-codegen",
+            "aas-core-codegen@git+https://github.com/aas-core-works/aas-core-codegen@67f789e#egg=aas-core-codegen",
             "astpretty==3.0.0",
             "pygments>=2,<3"
         ],
