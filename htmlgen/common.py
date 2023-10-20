6694f2baaa8dd0691edc969101c741f4289d1f9e
Marko Ristin <marko.ristin@gmail.com>
2023-10-20 11:58:01 +0200

    Change naming in htmlgen
    We change the naming so that URIs can be used as URLs from
    `https://admin-shell.io/AAS/3/0` and future versions.
    
    At this point, we also implement code highlighting ourselves, so that
    the properties, enumerations and constants are clickable in the code.

--- htmlgen/common.py
+++ htmlgen/common.py
@@ -1,6 +1,35 @@
 """Provide common elements used between the generators."""
+from aas_core_codegen import intermediate
+from aas_core_codegen.common import Stripped, assert_never
+
+import htmlgen.naming
 
 I = "  "
 II = I * 2
 III = I * 3
 IIII = I * 4
+
+
+def _render_type_annotation_recursively(
+    type_annotation: intermediate.TypeAnnotationUnion,
+) -> Stripped:
+    """Render the type annotation as HTML, without enclosing ``<span>``."""
+    if isinstance(type_annotation, intermediate.PrimitiveTypeAnnotation):
+        return Stripped(type_annotation.a_type.value)
+    elif isinstance(type_annotation, intermediate.OurTypeAnnotation):
+        name = htmlgen.naming.of(type_annotation.our_type)
+        return Stripped(f'<a href="{name}.html">{name}</a>')
+    elif isinstance(type_annotation, intermediate.ListTypeAnnotation):
+        items_type_anno = _render_type_annotation_recursively(type_annotation.items)
+        return Stripped(f"List[{items_type_anno}]")
+    elif isinstance(type_annotation, intermediate.OptionalTypeAnnotation):
+        value_type_anno = _render_type_annotation_recursively(type_annotation.value)
+        return Stripped(f"Optional[{value_type_anno}]")
+    else:
+        assert_never(type_annotation)
+
+
+def type_annotation_html(type_annotation: intermediate.TypeAnnotationUnion) -> Stripped:
+    """Render the type annotation as a ``<span>``."""
+    content = _render_type_annotation_recursively(type_annotation)
+    return Stripped(f'<span class="aas-type-annotation">{content}</span>')
---
+++ htmlgen/naming.py
@@ -0,0 +1,179 @@
+"""Generate HTML identifiers based on the identifiers from the meta-model."""
+from typing import Union
+
+import aas_core_codegen.naming
+from aas_core_codegen import intermediate
+from aas_core_codegen.common import Identifier, assert_never
+
+
+def class_name(identifier: Identifier) -> Identifier:
+    """
+    Generate an HTML class name based on its meta-model ``identifier``.
+
+    >>> class_name(Identifier("something"))
+    'Something'
+
+    >>> class_name(Identifier("URL_to_something"))
+    'UrlToSomething'
+    """
+    return aas_core_codegen.naming.capitalized_camel_case(identifier)
+
+
+def enum_name(identifier: Identifier) -> Identifier:
+    """
+    Generate an HTML enum name based on its meta-model ``identifier``.
+
+    >>> enum_name(Identifier("something"))
+    'Something'
+
+    >>> enum_name(Identifier("URL_to_something"))
+    'UrlToSomething'
+    """
+    return aas_core_codegen.naming.capitalized_camel_case(identifier)
+
+
+def enum_literal_name(identifier: Identifier) -> Identifier:
+    """
+    Generate an HTML name for an enum literal based on its meta-model ``identifier``.
+
+    >>> enum_literal_name(Identifier("something"))
+    'Something'
+
+    >>> enum_literal_name(Identifier("URL_to_something"))
+    'UrlToSomething'
+    """
+    return aas_core_codegen.naming.capitalized_camel_case(identifier)
+
+
+def property_name(identifier: Identifier) -> Identifier:
+    """
+    Generate a name for a property based on its meta-model ``identifier``.
+
+    >>> property_name(Identifier("something"))
+    'something'
+
+    >>> property_name(Identifier("something_to_URL"))
+    'somethingToUrl'
+    """
+    return aas_core_codegen.naming.lower_camel_case(identifier)
+
+
+def method_name(identifier: Identifier) -> Identifier:
+    """
+    Generate a name for a member method based on its meta-model ``identifier``.
+
+    >>> method_name(Identifier("do_something"))
+    'DoSomething'
+
+    >>> method_name(Identifier("do_something_to_URL"))
+    'DoSomethingToUrl'
+    """
+    return aas_core_codegen.naming.capitalized_camel_case(identifier)
+
+
+def argument_name(identifier: Identifier) -> Identifier:
+    """
+    Generate a name for an argument based on its meta-model ``identifier``.
+
+    >>> argument_name(Identifier("something"))
+    'something'
+
+    >>> argument_name(Identifier("something_to_URL"))
+    'somethingToUrl'
+    """
+    return aas_core_codegen.naming.lower_camel_case(identifier)
+
+
+def function_name(identifier: Identifier) -> Identifier:
+    """
+    Generate a name for a function based on its meta-model ``identifier``.
+
+    >>> function_name(Identifier("do_something"))
+    'DoSomething'
+
+    >>> function_name(Identifier("do_something_to_URL"))
+    'DoSomethingToUrl'
+    """
+    return aas_core_codegen.naming.capitalized_camel_case(identifier)
+
+
+def constrained_primitive(identifier: Identifier) -> Identifier:
+    """
+    Generate a name for a constrained primitive based on its meta-model ``identifier``.
+
+    >>> constrained_primitive(Identifier("something"))
+    'Something'
+
+    >>> constrained_primitive(Identifier("something_to_URL"))
+    'SomethingToUrl'
+    """
+    return aas_core_codegen.naming.capitalized_camel_case(identifier)
+
+
+def constant_name(identifier: Identifier) -> Identifier:
+    """
+    Generate a name for a constant based on its meta-model ``identifier``.
+
+    >>> constant_name(Identifier("something"))
+    'Something'
+
+    >>> constant_name(Identifier("something_to_URL"))
+    'SomethingToUrl'
+    """
+    return aas_core_codegen.naming.capitalized_camel_case(identifier)
+
+
+def variable_name(identifier: Identifier) -> Identifier:
+    """
+    Generate a name for a variable in code.
+
+    >>> variable_name(Identifier("something"))
+    'something'
+
+    >>> constant_name(Identifier("something_to_URL"))
+    'somethingToUrl'
+    """
+    return aas_core_codegen.naming.lower_camel_case(identifier)
+
+
+def of(
+    something: Union[
+        intermediate.Enumeration,
+        intermediate.EnumerationLiteral,
+        intermediate.Class,
+        intermediate.Verification,
+        intermediate.Property,
+        intermediate.Method,
+        intermediate.ConstrainedPrimitive,
+        intermediate.Constant,
+    ]
+) -> Identifier:
+    """Dispatch to the appropriate naming function."""
+    if isinstance(something, intermediate.Enumeration):
+        return enum_name(something.name)
+
+    elif isinstance(something, intermediate.EnumerationLiteral):
+        return enum_name(something.name)
+
+    elif isinstance(something, intermediate.Class):
+        return class_name(something.name)
+
+    elif isinstance(something, intermediate.Verification):
+        return function_name(something.name)
+
+    elif isinstance(something, intermediate.Property):
+        return property_name(something.name)
+
+    elif isinstance(something, intermediate.Method):
+        return method_name(something.name)
+
+    elif isinstance(something, intermediate.ConstrainedPrimitive):
+        return constrained_primitive(something.name)
+
+    elif isinstance(something, intermediate.Constant):
+        return constant_name(something.name)
+
+    else:
+        assert_never(something)
+
+    raise AssertionError("Should not have gotten here")
